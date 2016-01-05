5b190b241e071c2475a869d077f9fe8e3a40f116
iceStone <ice@micua.com>
2016-01-05 17:13:08 +0800

    initial commit

--- wedn.php
+++ wedn.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: wedn
  * Plugin URI: http://wordpress.org/plugins/wedn/
- * Description: 禁用Google字体，修改Gravatar为多说服务提供
+ * Description: 修改Google字体服务为360提供，修改Gravatar为多说服务提供
  * Author: iceStone <ice@wedn.net>
  * Version: 1.0
  * Author URI: http://wedn.net/
@@ -11,9 +11,9 @@
 /**
  * 载入过滤器
  */
-require_once dirname( __FILE__ ) . 'includes/filters.php';
+require_once dirname( __FILE__ ) . '/includes/filters.php';
 
 /**
  * 载入所有行为
  */
-require_once dirname( __FILE__ ) . 'includes/actions.php';
+require_once dirname( __FILE__ ) . '/includes/actions.php';
